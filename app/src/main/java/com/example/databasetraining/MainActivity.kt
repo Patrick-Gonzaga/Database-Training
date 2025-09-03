21e7781f14b56ef1d665fdb3f30adb34494a3421
Patrick <patrickdasilvagonzaga@gmail.com>
2025-09-03 11:30:09 -0300

    testando update e remove

--- app/src/main/java/com/example/databasetraining/MainActivity.kt
+++ app/src/main/java/com/example/databasetraining/MainActivity.kt
@@ -22,17 +22,64 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
-
         with(binding){
-            val email = inputEmail.text.toString()
-            val password = inputPassword.text.toString()
-            buttonSave.setOnClickListener { saveDB(email, password) }
+            buttonSave.setOnClickListener {
+                val email = inputEmail.text.toString()
+                val password = inputPassword.text.toString()
+                saveDB(email, password)
+            }
             buttonList.setOnClickListener { listDB() }
+            buttonUpdate.setOnClickListener {
+                val email = inputEmail.text.toString()
+                val password = inputPassword.text.toString()
+                updateDB(email, password) }
+            buttonRemove.setOnClickListener { removeItemDB() }
+        }
+
+    }
+
+    private fun removeItemDB() {
+        val sql = "DELETE FROM ${Database.TABELA_USERS} " +
+                "WHERE ${Database.ID_USER} = (SELECT ${Database.ID_USER} " +
+                "FROM ${Database.TABELA_USERS} " +
+                "ORDER BY ${Database.ID_USER} DESC LIMIT 1);"
+
+        try {
+            database.writableDatabase.execSQL(sql)
+            Log.i("log_db", "instExecSQL: Sucesso ao remover")
+        }catch (e: Exception){
+            Log.i("log_db", "instExecSQL: Falhou ao remover || ${e.message}")
+        }
+    }
+
+    private fun updateDB(email: String, password: String) {
+        val sql = "UPDATE ${Database.TABELA_USERS} SET " +
+                "${Database.EMAIL} = '$email', password = '$password' " +
+                "WHERE ${Database.ID_USER} = 1;"
+
+        try {
+            database.writableDatabase.execSQL(sql)
+            Log.i("log_db", "instExecSQL: Sucesso ao atualizar")
+        }catch (e: Exception){
+            Log.i("log_db", "instExecSQL: Falhou ao atualizar || ${e.message}")
         }
 
     }
 
     private fun listDB() {
+        val sql = "SELECT * FROM ${Database.TABELA_USERS};"
+        val cursor = database.readableDatabase.rawQuery(sql, null)
+
+        val iId = cursor.getColumnIndex(Database.ID_USER)
+        val iEmail = cursor.getColumnIndex(Database.EMAIL)
+        val iPassword = cursor.getColumnIndex(Database.PASSWORD)
+
+        while (cursor.moveToNext()){
+            val idUser = cursor.getInt(iId)
+            val email = cursor.getString(iEmail)
+            val password = cursor.getString(iPassword)
+            Log.i("log_db", "position: ${cursor.position} | id: $idUser | email: $email | password: $password")
+        }
 
     }
 
--- app/src/main/java/com/example/databasetraining/database/Database.kt
+++ app/src/main/java/com/example/databasetraining/database/Database.kt
@@ -5,13 +5,20 @@ import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
 import android.util.Log
 
-class Database(context: Context): SQLiteOpenHelper(context, "test.db", null, 2) {
+
+class Database(context: Context): SQLiteOpenHelper(context, "test.db", null, 1) {
+    companion object{
+        const val TABELA_USERS = "users"
+        const val ID_USER = "id_user"
+        const val EMAIL = "email"
+        const val PASSWORD = "password"
+    }
     override fun onCreate(db: SQLiteDatabase?) {
         Log.i("log_db", "onCreate: Sucesso")
-        val sqlCode = "CREATE TABLE IF NOT EXISTS users(\n" +
-                "  id_user integer NOT NULL PRIMARY KEY AUTOINCREMENT, \n" +
-                "  email varchar(40),\n" +
-                "  password varchar(40)\n" +
+        val sqlCode = "CREATE TABLE IF NOT EXISTS $TABELA_USERS(\n" +
+                "  $ID_USER integer NOT NULL PRIMARY KEY AUTOINCREMENT, \n" +
+                "  $EMAIL varchar(40),\n" +
+                "  $PASSWORD varchar(40)\n" +
                 ");"
 
         try {
@@ -29,7 +36,7 @@ class Database(context: Context): SQLiteOpenHelper(context, "test.db", null, 2) 
         oldVersion: Int,
         newVersion: Int
     ) {
-        Log.i("log_db", "onUpgrade: Sucesso")
+        Log.i("log_db", "old: ${oldVersion} | new: ${newVersion} | onUpgrade: Sucesso")
 
     }
 }
